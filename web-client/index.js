63083a14b5eff2201368736955cbc8cdecce6e22
Ludwig Stumpp <42147848+LudwigStumpp@users.noreply.github.com>
2021-09-07 22:46:14 +0200

    First prototype of client

---
+++ web-client/index.js
@@ -0,0 +1,66 @@
+let storage = null;
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        delta: 0.2,
+        noExercise: 'uncertain',
+        log: '',
+        tracker: {},
+        serviceUUID: '',
+        isConnected: false,
+        isRecording: false,
+        myCharacteristic: null,
+        myBLE: null,
+    },
+    created() {
+        this.myBLE = new p5ble();
+    },
+    computed: {
+        hasTrackingData: function () {
+            dict = this.tracker;
+            return Object.keys(dict).length > 0;
+        },
+    },
+    mounted: function () {
+    },
+    methods: {
+        connectBLE: function () {
+            this.myBLE.connect(this.serviceUUID, this.gotCharacteristics);
+        },
+        disconnectBLE: function () {
+            this.myBLE.disconnect();
+            this.isConnected = this.myBLE.isConnected();
+        },
+        gotCharacteristics: function (error, characteristics) {
+            if (error) console.log('error: ', error);
+            this.myCharacteristic = characteristics[0];
+
+            this.isConnected = this.myBLE.isConnected();
+            this.myBLE.onDisconnected(this.disconnectBLE);
+
+            this.myBLE.startNotifications(this.myCharacteristic, this.handleNotifications, 'string');
+        },
+        handleNotifications: function (data) {
+            this.log = data;
+            if (this.isRecording && data != this.noExercise) {
+                if (data in this.tracker) {
+                    curr = this.tracker[data];
+                    this.tracker[data] = Math.round((curr + parseFloat(this.delta)) * 10) / 10;
+                } else {
+                    Vue.set(this.tracker, data, parseFloat(this.delta));
+                }
+            }
+        },
+        startRecording: function () {
+            this.isRecording = true;
+        },
+        stopRecording: function () {
+            this.isRecording = false;
+        },
+        resetRecording: function () {
+            this.isRecording = false;
+            this.tracker = {};
+        },
+    }
+})
